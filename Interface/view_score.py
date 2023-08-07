c94def9caff4a92f9978f4ea6a335cbb542b564f
CarlosChiva <cxhiva@gmail.com>
2023-08-07 17:42:19 +0200

    add new game into view_score

--- Interface/view_score.py
+++ Interface/view_score.py
@@ -1,19 +1,20 @@
 from tkinter import Canvas, Frame, Tk
 import tkinter
+import Game
 from scores import Scores
 
 class ViewScore():
     def __init__(self,root):
         self.root = root 
-        etiqueta_titulo = tkinter.Label(self.root, text="Puntajes de la Partida", font=("Helvetica", 16, "bold"))
-        etiqueta_titulo.grid(row=0, column=0, columnspan=2, pady=10)
+        self.etiqueta_titulo = tkinter.Label(self.root, text="Puntajes de la Partida", font=("Helvetica", 16, "bold"))
+        self.etiqueta_titulo.grid(row=0, column=0, columnspan=2, pady=10)
 
-        lista_scores = tkinter.Listbox(self.root, font=("Helvetica", 12), width=30)
-        lista_scores.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
-        lista_scores.config(justify=tkinter.CENTER)
+        self.lista_scores = tkinter.Listbox(self.root, font=("Helvetica", 12), width=30)
+        self.lista_scores.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+        self.lista_scores.config(justify=tkinter.CENTER)
         self.score = Scores()
         for score in self.score.scores:
-            lista_scores.insert(tkinter.END, score)
+            self.lista_scores.insert(tkinter.END, score)
         # Expandir filas y columnas para centrar los elementos
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_rowconfigure(1, weight=1)
@@ -21,9 +22,14 @@ class ViewScore():
         self.root.grid_columnconfigure(1, weight=1)
 
         # Agregar botones en las esquinas inferiores
-        boton_izquierda = tkinter.Button(self.root, text="New Game")
-        boton_izquierda.grid(row=2, column=0, padx=10, pady=5, sticky=tkinter.S+tkinter.W)
+        self.boton_izquierda = tkinter.Button(self.root, text="New Game",command=self.newGame)
+        self.boton_izquierda.grid(row=2, column=0, padx=10, pady=5, sticky=tkinter.S+tkinter.W)
 
-        boton_derecha = tkinter.Button(self.root, text="Exit",command=exit)
-        boton_derecha.grid(row=2, column=1, padx=10, pady=5, sticky=tkinter.S+tkinter.E)
-    
+        self.boton_derecha = tkinter.Button(self.root, text="Exit",command=exit)
+        self.boton_derecha.grid(row=2, column=1, padx=10, pady=5, sticky=tkinter.S+tkinter.E)
+    def newGame(self):
+        self.etiqueta_titulo.destroy()
+        self.lista_scores.destroy()
+        self.boton_izquierda.destroy()
+        self.boton_derecha.destroy()
+        game = Game.Game(self.root)
