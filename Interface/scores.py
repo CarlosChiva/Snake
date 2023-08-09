8a53bbbd42dce6b4d81f643556946933bc2cabaf
CarlosChiva <cxhiva@gmail.com>
2023-08-09 18:22:16 +0200

    configuration to view_score and sorted of scores

--- Interface/scores.py
+++ Interface/scores.py
@@ -2,7 +2,9 @@ import pickle
 import datetime
 class Scores():
     def __init__(self):
-        self.scores = self.read_scores()
+        self.scores = self.__read_scores()
+        print(self.scores)
+        print("------------------------------------")
     def write_score(self, new_score):
         current_date = datetime.datetime.now()
         self.scores[current_date] = new_score
@@ -10,7 +12,7 @@ class Scores():
         with open("Scores", "wb") as file:
             pickle.dump(self.scores, file)
     
-    def read_scores(self):
+    def __read_scores(self):
         try:
             with open("Scores", "rb") as file:
                 scores = pickle.load(file)
@@ -18,4 +20,9 @@ class Scores():
         except FileNotFoundError:
             scores = {}
         return scores
-        
+    def __get_sorted_scores(self):
+        sorted_scores = sorted(self.scores.items(), key=lambda x: x[1], reverse=True)
+        print(dict(sorted_scores))
+        return dict(sorted_scores)
+    def get_scores(self):
+        return self.__get_sorted_scores() 
--- Interface/view_score.py
+++ Interface/view_score.py
@@ -6,24 +6,34 @@ from scores import Scores
 class ViewScore():
     def __init__(self,root):
         self.root = root 
-        self.etiqueta_titulo = tkinter.Label(self.root, text="Puntajes de la Partida", font=("Helvetica", 16, "bold"))
-        self.etiqueta_titulo.grid(row=0, column=0, columnspan=2, pady=10)
-
-        self.lista_scores = tkinter.Listbox(self.root, font=("Helvetica", 12), width=30)
-        self.lista_scores.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        # main frame
+        self.main_frame = Frame(self.root, bg="green")
+        self.main_frame.grid(row=0, column=0, sticky="nsew")
+        # Configurar cómo las filas y columnas dentro de main_frame se expanden
+        self.main_frame.grid_rowconfigure(0, weight=0)  # No necesitamos que la primera fila dentro de main_frame expanda
+        self.main_frame.grid_rowconfigure(1, weight=1)  # Queremos que la segunda fila dentro de main_frame expanda
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        
+        
+        self.etiqueta_titulo = tkinter.Label(self.main_frame, text="Scores", font=("Helvetica", 16, "bold"))
+        self.etiqueta_titulo.grid(row=0, column=0, columnspan=2, pady=5,sticky="n")
+        self.lista_scores = tkinter.Listbox(self.main_frame, font=("Helvetica", 12), width=30)
+        self.lista_scores.grid(row=1, column=0, columnspan=2, padx=10, pady=100,sticky="n")
         self.lista_scores.config(justify=tkinter.CENTER)
+  
         self.score = Scores()
-        for date, score in self.score.scores.items():
+        for date, score in self.score.get_scores().items():
             date_str = date.strftime("%Y-%m-%d %H:%M:%S")
             self.lista_scores.insert(tkinter.END, f"{date_str} - Score: {score}")
-        # Expandir filas y columnas para centrar los elementos
      
 
         # Agregar botones en las esquinas inferiores
-        self.boton_izquierda = tkinter.Button(self.root, text="New Game",command=self.newGame)
+        self.boton_izquierda = tkinter.Button(self.main_frame, text="New Game",command=self.newGame)
         self.boton_izquierda.grid(row=2, column=0, padx=10, pady=5, sticky=tkinter.S+tkinter.W)
 
-        self.boton_derecha = tkinter.Button(self.root, text="Exit",command=exit)
+        self.boton_derecha = tkinter.Button(self.main_frame, text="Exit",command=exit)
         self.boton_derecha.grid(row=2, column=1, padx=10, pady=5, sticky=tkinter.S+tkinter.E)
     def newGame(self):
         self.etiqueta_titulo.destroy()
