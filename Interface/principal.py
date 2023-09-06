f8f5fbb41bdb84d6aa218851b86639cba0ae7e73
CarlosChiva <cxhiva@gmail.com>
2023-09-06 17:25:57 +0200

    change pack for  grid in principal window

--- Interface/principal.py
+++ Interface/principal.py
@@ -4,21 +4,28 @@ class Principal():
         def __init__(self,root,change_window):
                 self.root = root
                 self.function = change_window
-                self.frame1 = Frame(self.root, width=700, height=500, background="white")
-                self.frame2 = Frame(self.frame1, width=100, height=100)
-                self.secondLabel = Label(self.frame1, text="Welcome", fg="red", font=("Arial", 18))
-                self.secondLabel.place(x=285, y=100)
-                self.newGameButton = Button(self.frame2, text="New Game",command=self.buildGame)
-                self.newGameButton.grid(row=100, column=0, pady=20, padx=10)
-                self.newGameButton2 = Button(self.frame2, text="Load Game",command=self.build_load_game)
-                self.newGameButton2.grid(row=100, column=1, pady=20, padx=10)
-                self.newGameButton3 = Button(self.frame2, text="View Scores",command=self.build_view_score)
-                self.newGameButton3.grid(row=100, column=2, pady=20, padx=10)
-                self.frame1.pack()
-                self.frame2.pack()
+                self.root.grid_rowconfigure(0, weight=1)
+                self.root.grid_columnconfigure(0, weight=1)
+                # main frame
+                self.main_frame = Frame(self.root, bg="green")
+                self.main_frame.grid(row=0, column=0, sticky="nsew")
+                # Configurar cómo las filas y columnas dentro de main_frame se expanden
+             
+                
+                self.secondLabel = Label(self.main_frame, text="Welcome",background="green", fg="red", font=("Arial", 18))
+                
+                self.secondLabel.grid(row=0, column=1,pady=50)
+                
+                self.newGameButton = Button(self.main_frame, text="New Game",command=self.buildGame)
+                self.newGameButton.grid(row=1, column=0,pady=50,padx=70)
+                self.newGameButton2 = Button(self.main_frame, text="Load Game",command=self.build_load_game)
+                self.newGameButton2.grid(row=1, column=1,padx=70)
+                self.newGameButton3 = Button(self.main_frame, text="View Scores",command=self.build_view_score)
+                self.newGameButton3.grid(row=1, column=2,padx=70)
+
         def destroy_main_frames(self):
-                self.frame1.destroy()
-                self.frame2.destroy()
+                self.main_frame.destroy()
+        
         def buildGame(self):
                 self.destroy_main_frames()
                 self.function("game")     
